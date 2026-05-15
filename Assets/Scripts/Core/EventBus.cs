4c01a10d913f79a085298a7f46b1d8b5acfd8601
Emir Berat Usta <ebusta3333@gmail.com>
2026-05-15 20:51:57 +0300

    feat: add EventBus for Observer event channels

---
+++ Assets/Scripts/Core/EventBus.cs
@@ -0,0 +1,17 @@
+using System;
+using UnityEngine;
+
+public static class EventBus
+{
+    public static event Action<float, float> OnCoreDamaged;
+    public static event Action OnCoreDestroyed;
+    public static event Action<Vector3> OnEnemyDied;
+    public static event Action<int> OnWaveCleared;
+    public static event Action<GameState> OnGameStateChanged;
+
+    public static void CoreDamaged(float current, float max) => OnCoreDamaged?.Invoke(current, max);
+    public static void CoreDestroyed() => OnCoreDestroyed?.Invoke();
+    public static void EnemyDied(Vector3 position) => OnEnemyDied?.Invoke(position);
+    public static void WaveCleared(int waveIndex) => OnWaveCleared?.Invoke(waveIndex);
+    public static void GameStateChanged(GameState state) => OnGameStateChanged?.Invoke(state);
+}
